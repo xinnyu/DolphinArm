a6401a515d4b05cd396dea73bac6bf6afcd3966b
pxy <panxinyu@mgtv.com>
2023-08-28 17:31:43 +0800

    Initial Commit

---
+++ DolphinArm/DolphinArmApp.swift
@@ -0,0 +1,17 @@
+//
+//  DolphinArmApp.swift
+//  DolphinArm
+//
+//  Created by 潘新宇 on 2023/8/28.
+//
+
+import SwiftUI
+
+@main
+struct DolphinArmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
